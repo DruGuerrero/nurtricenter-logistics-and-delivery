d4e1c00feeaa389859f59b85b3209e2152d862ce
Andres Sebastian Guerrero Camacho <andres1druchito2@gmail.com>
2026-07-08 21:17:03 -0400

    add dispatch domain events in commit async

--- src/Nurtricenter.Infrastructure/Data/UnitOfWork.cs
+++ src/Nurtricenter.Infrastructure/Data/UnitOfWork.cs
@@ -1,16 +1,41 @@
 namespace Nurtricenter.Infrastructure.Data;
 
 using Joseco.DDD.Core.Abstractions;
+using MediatR;
 
 public sealed class UnitOfWork : IUnitOfWork
 {
     private readonly AppDbContext _context;
+    private readonly IMediator _mediator;
 
-    public UnitOfWork(AppDbContext context)
+    public UnitOfWork(AppDbContext context, IMediator mediator)
     {
         _context = context;
+        _mediator = mediator;
     }
 
     public async Task CommitAsync(CancellationToken cancellationToken = default)
-        => await _context.SaveChangesAsync(cancellationToken);
+    {
+        var entitiesWithEvents = _context.ChangeTracker
+            .Entries<Entity>()
+            .Where(e => e.Entity.DomainEvents.Any())
+            .Select(e => e.Entity)
+            .ToList();
+
+        var domainEvents = entitiesWithEvents
+            .SelectMany(e => e.DomainEvents)
+            .ToList();
+
+        await _context.SaveChangesAsync(cancellationToken);
+
+        foreach (var domainEvent in domainEvents)
+        {
+            await _mediator.Publish(domainEvent, cancellationToken);
+        }
+
+        foreach (var entity in entitiesWithEvents)
+        {
+            entity.ClearDomainEvents();
+        }
+    }
 }
